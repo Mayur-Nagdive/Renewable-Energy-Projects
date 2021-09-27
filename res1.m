fe7f7152fba74541fed6eec6c8a48047e7ec30e0
Mayur Nagdive <81313227+Mayur-Nagdive@users.noreply.github.com>
2021-09-27 22:47:59 +0530

    Add files via upload

---
+++ res1.m
@@ -0,0 +1,15 @@
+%Experiment 1   
+%VI and PV characteristics of solar cell
+%grp: Mayur Nagdive, Ganesh Dhavan, Devdutta Dokhale, Hemant Chaudhary
+
+% Run the code for different values of  PS and then run this code...
+...upto 5 values are supported in this code but you can increase...
+ ...that by adding more variables
+plot(voltage1,current1,voltage2,current2,voltage3,current3,voltage4,current4,voltage5,current5);
+xlabel('Voltage(V)');
+ylabel('Current(I)');
+title('V-I curve of solar cell');
+plot(voltage1,power1,voltage2,power2,voltage3,power3,voltage4,power4,voltage5,power5);
+xlabel('Voltage(V)');
+ylabel('Current(I)');
+title('V-I curve of solar cell');
